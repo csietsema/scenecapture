d3fdf547b58d325c0fc70f336733c4641bd7d468
mikaeltyrsen <mikael@nucontext.com>
2018-05-15 08:50:35 -0700

    Create config.php

---
+++ inc/config.php
@@ -0,0 +1,9 @@
+<?
+	
+	// webCoRE Run Scene Piston URL
+	$webcoreUrlRun = "URL_TO_RUN_SCENE_PISTON_HERE";
+	
+	// webCoRE Get Scene Piston URL
+	$webcoreUrlGet = "URL_TO_GET_SCENE_PISTON_HERE";
+	
+?>
